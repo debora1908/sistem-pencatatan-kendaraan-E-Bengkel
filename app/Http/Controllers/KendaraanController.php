8d23ffe310b2d6c9cc6f4b3a4a6fd2693b85181f
Debora1908 <corysimarmata19@gmail.com>
2026-05-13 02:14:22 +0700

    Membuat daftar services dengan tabel Bootstrap dan tombol tambah

--- app/Http/Controllers/KendaraanController.php
+++ app/Http/Controllers/KendaraanController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kendaraan;
 use Illuminate\Http\Request;
 
 class KendaraanController extends Controller
 {
-    //
+    // Fungsi index untuk mengambil data dari database
+    public function index()
+    {
+        $kendaraans = Kendaraan::all();
+        return view('kendaraan.index', compact('kendaraans'));
+    }
 }
