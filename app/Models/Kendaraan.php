3bedf12df550b50e5b139861f273047ffe4070b0
Debora1908 <corysimarmata19@gmail.com>
2026-05-13 01:33:37 +0700

    Konfigurasi Model

---
+++ app/Models/Kendaraan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kendaraan extends Model
+{
+    use HasFactory;
+
+    
+    protected $fillable = ['plat_nomor', 'nama_pemilik', 'merk_kendaraan', 'keluhan'];
+}
